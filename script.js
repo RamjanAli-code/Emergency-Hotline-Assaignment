bf3c4708e6a79da648779929816451bfa01780e9
RamjanAli-code <ra3939753@gmail.com>
2025-08-29 04:49:22 +0600

    seventh commit

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ for (let i = 0; i < heartsec.length; i++) {
     callingsec[i].addEventListener("click", function() {
         if (coin < 20) {
             alert(" ⚠️ Warning! Coin less than 20");
+            return;
         } else {
             const cardsec = callingsec[i].closest(".card");
             const alertname = cardsec.querySelector("p").textContent;
@@ -23,5 +24,20 @@ for (let i = 0; i < heartsec.length; i++) {
             coin -= 20;
             coinsec.textContent = coin;
         }
+        const containersec = document.getElementById("container");
+        const addcontainer = document.createElement("div");
+        const cards = callingsec[i].closest(".card");
+        const servicename = cards.querySelector("p").textContent;
+        const servicenumber = cards.querySelector("h3").textContent;
+        addcontainer.innerHTML = `
+        <div class=" bg-gray-300 w-full h-[70px] rounded-xl flex flex-col my-3 px-3 ">
+        <p class="text-[#5C5C5C] font-bold text-xl ">${servicename}</p>
+        <h3 class="font-bold pb-1 text-2xl">${servicenumber}</h3>
+
+</div> `;
+        containersec.appendChild(addcontainer);
+        document.getElementById("clear-btn").addEventListener("click", function() {
+            containersec.innerHTML = "";
+        });
     });
 }
